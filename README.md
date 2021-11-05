fa2133d33dee74e155133681ff75cae574befd99
Laercio de Souza Rios <laercio.rios13@hotmail.com>
2021-11-05 02:27:49 -0300

    :sparkles: Added todos CRUD

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Desafio 01 - Jornada Ignite (NodeJs) da Rockeseat
 
+<h4 align="center"> 
+	✅  Desafio Concluído  ✅
+</h4>
+
 ## Descrição do Projeto
 Este repositório contêm minha solução para o primeiro desafio da trilha de NodeJs da plataforma Ignite da [Rockeseat](https://www.rocketseat.com.br/).
 
@@ -8,11 +12,11 @@ Link com Detalhes: [Desafio 01 - Conceitos do Node.js](https://www.notion.so/Des
 
 ## Requisitos
 - [x] Criar um novo usuário;
-- [ ] Criar um novo *todo*;
-- [ ] Listar todos os *todos*;
-- [ ] Alterar o `title` e `deadline` de um *todo* existente;
-- [ ] Marcar um *todo* como feito;
-- [ ] Excluir um *todo*;
+- [x] Criar um novo *todo*;
+- [x] Listar todos os *todos*;
+- [x] Alterar o `title` e `deadline` de um *todo* existente;
+- [x] Marcar um *todo* como feito;
+- [x] Excluir um *todo*;
 
 
 ## Tecnologias
--- src/index.js
+++ src/index.js
@@ -12,10 +12,10 @@ const users = [];
 function checksExistsUserAccount(request, response, next) {
   const { username } = request.headers;
 
-  const user = users.filter((user) => user.username === username);
+  const user = users.find((user) => user.username === username);
 
   if (!user) {
-    return response.status(404).json({ error: "Mensagem do erro" });
+    return response.status(404).json({ error: "User not found!" });
   }
 
   request.user = user;
@@ -31,7 +31,7 @@ app.post("/users", (request, response) => {
   );
 
   if (usernameAlreadyExists) {
-    return response.status(404).json({ error: "Mensagem do erro" });
+    return response.status(400).json({ error: "Username already exists!" });
   }
 
   const user = {
@@ -43,27 +43,77 @@ app.post("/users", (request, response) => {
 
   users.push(user);
 
-  return response.json(user);
+  return response.status(201).json(user);
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  };
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { id } = request.params;
+  const { user } = request;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found!" });
+  }
+
+  todo.title = title;
+  todo.deadline = deadline;
+
+  return response.status(201).json(todo);
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+  const { user } = request;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found!" });
+  }
+
+  todo.done = true;
+
+  return response.status(201).json(todo);
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+  const { user } = request;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found!" });
+  }
+
+  user.todos.splice(todo, 1);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
